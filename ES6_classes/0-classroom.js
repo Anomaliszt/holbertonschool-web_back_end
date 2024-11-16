787f4e79450816e6801a570ab091de323776e3b7
Anomaliszt <xavier.sanchez44@orange.fr>
2024-11-16 22:46:48 +0100

    fix test

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,11 @@
-/* 0-classroom.js */
-
+/**
+ * Class representing a classroom.
+ */
 export default class ClassRoom {
+  /**
+   * Create a classroom.
+   * @param {number} maxStudentsSize - The maximum number of students allowed in the classroom.
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
