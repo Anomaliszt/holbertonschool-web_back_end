f165408b84f1127ba4ce35bdcf6c0e42885f9c0d
Anomaliszt <xavier.sanchez44@orange.fr>
2024-11-28 23:05:12 +0100

    fixed

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,5 +1,13 @@
 // try-catch
 
-function guardrail(mathfunction) {
-  queue = [];
+function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
 }
