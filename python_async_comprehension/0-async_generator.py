4cf369de716a02e13f58142f16ce1acb63584b03
Anomaliszt <xavier.sanchez44@orange.fr>
2024-12-10 09:37:31 +0100

    fix pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     Coroutine that loops 10 times,
     each time asynchronously waits 1 second,
