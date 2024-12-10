10a0f03517782809dcfae54fba37f7c3ab44a00b
Anomaliszt <xavier.sanchez44@orange.fr>
2024-12-10 09:46:24 +0100

    fixed pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,13 @@
 """ Async Generator """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
-    Coroutine that loops 10 times,
-    each time asynchronously waits 1 second,
-    then yields a random number between 0 and 10.
+    The coroutine will loop 10 times,
+    each time asynchronously wait 1 second,
     """
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Asnyc comprehension """
+""" Async comprenhesion gen """
+import asyncio
 from typing import List
 
 
@@ -8,7 +9,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Collect 10 random numbers using
-    an async comprehensing over async_generator 
+    The coroutine will collect 10 random numbers using an async comprehensing
     """
-    return [number async for number in async_generator()]
+    return ([i async for i in async_generator()])
