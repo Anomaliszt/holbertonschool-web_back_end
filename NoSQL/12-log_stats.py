ab187788d66d02c7e8e462247bb66e6789bc5db1
Anomaliszt <xavier.sanchez44@orange.fr>
2024-12-13 18:37:41 +0100

    refactor log_stats.py to improve readability and structure

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-""" Count the number of logs in the nginx
-collection and the number of logs for each method
-"""
+""" Log stats """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+def log_stats():
+    """ Log stats """
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-    total_logs = nginx_collection.count_documents({})
+    total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_checks} status check")
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    log_stats()
