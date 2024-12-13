1277b080a4d6bfa26ccab51d8bea295d85ed698e
Anomaliszt <xavier.sanchez44@orange.fr>
2024-12-13 18:42:05 +0100

    refactor log_stats.py for improved structure and clarity

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,23 +2,21 @@
 """ Log stats """
 from pymongo import MongoClient
 
-def log_stats():
-    """ Log stats """
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.nginx
-    collection = db.logs
+    db = client.logs
+    collection = db.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print(f"{collection.count_documents({})} logs")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
     print(f"{status_check} status check")
-
-if __name__ == "__main__":
-    log_stats()
